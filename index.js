317d2187a36e61f222a655e3c0b62caf52bd1f2c
jaikrishnaverma <jaikrishnaverma>
2022-08-09 11:00:16 +0530

    stopwatch change date update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var stopwatch=1;
 var d;
 var t = [{year:0,month:0,day:0, hour: 0, minute: 0, second: 0, milisecond: 0 }];
 const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  
+
 var setter=setInterval(function () {
     d=new Date(Date.now());
     render(d)
@@ -65,32 +65,79 @@ else
 stopwatch++;
 });
 var custom = [{year:0,month:0,day:0, hours: 0, minutes: 0, seconds: 0}];
+const mday=[31,28,31,30,31,30,31,31,30,31,30,31];
+var flag=1;
+var stateInt;
 $("#cbtn").click(function () {
-   custom[0].year=$("#year").val() ;
-   custom[0].month=$("#month").val() ;
-   custom[0].day=$("#day").val() ;
-   custom[0].hours=$("#hours").val() ;
-   custom[0].minutes=$("#minutes").val() ;
+    clearInterval(stateInt);
+    
+    custom[0].year=parseInt($("#year").val()) ;
+    custom[0].month=parseInt($("#month").val()) -1;
+        if(custom[0].month>12)
+            return alert("month value cannot be greater than 12.");
+    custom[0].day=parseInt($("#day").val()) ;
+        if(custom[0].day>mday[custom[0].month])
+            return alert("This Month have maximum No. of Days is: "+mday[custom[0].month]);
+    custom[0].hours=parseInt($("#hours").val());
+        if(custom[0].hours>23)
+        return alert("Hours value must lie between 0 to 23.");
+    custom[0].minutes=parseInt($("#minutes").val()) ;
+        if(custom[0].minutes>59)
+        return alert("Minutes value must lie between 0 to 59.");
 
 clearInterval(setter);
-setInterval(function () {
-    render(new Date(custom[0].year, custom[0].month, custom[0].day, custom[0].hours, custom[0].minutes,custom[0].seconds));
+stateInt=setInterval(function () {
+    var y=new Date(custom[0].year, custom[0].month, custom[0].day, custom[0].hours, custom[0].minutes,custom[0].seconds);
+    y.toLocaleString('en-GB')
+    render(y);
    
-    if (custom[0].seconds == 60) {
+    if (custom[0].seconds >59) {
         custom[0].minutes += 1;
-        custom[0].seconds = 0
+        custom[0].seconds = 0;
+        console.log("This is hour: "+custom[0].hours)
     }
-    if (custom[0].minutes == 60) {
+    if (custom[0].minutes > 59) {
         custom[0].hours += 1;
-        custom[0].minutes = 0
+        custom[0].minutes = 0;
+
+        console.log("This is hour: "+custom[0].hours)
     }
-    if (custom[0].hours == 13) {
-        custom[0].hours =1;
-        custom[0].day+=1;
+    if (custom[0].hours > 23) {
+        if (custom[0].hours == 24 && custom[0].minutes ==0 && custom[0].seconds ==0)
+         custom[0].day+=1;
+
+
+        custom[0].hours =0;
+        console.log("This is day: "+custom[0].day)
         
+    }
+    if (custom[0].day > mday[custom[0].month]) {
+        custom[0].day =1;
+        custom[0].month+=1;
+    }
+    if (custom[0].month > 12) {
+        custom[0].month =1;
+        custom[0].year+=1;
+    }
+    if(checkLeapYear(custom[0].year)=="yes")
+    {
+         mday[1]=29;
+    }
+    else{
+        mday[1]=28;
     }
     custom[0].seconds+=1;
 }, 1000);
+function checkLeapYear(year) {
 
+    //three conditions to find out the leap year
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        return "yes";
+    } else {
+        return "not";
+    }
+}
 
-})
+}
+
+)
